136a11cd8f39650e192c76763f5e92254d5fabb9
Samuel Teuber <teuber@kit.edu>
2025-11-22 00:06:51 +0100

    Complated dnf tests

--- test/dnf.jl
+++ test/dnf.jl
@@ -46,12 +46,121 @@ end
             parse_query_str(content) do (ast)
                 dnf = to_dnf(get_bool(ast))
                 expected = [[ "(<= X [0] 10.0) ", "(>= X [1] 5.0) "]]
-                println(map(x->typeof.(x),dnf))
                 assert_dnf_equals(expected, dnf)
             end
         end
         @testset "test_three_way_conjunction" begin
-            
+            content = """
+            (vnnlib-version <2.0>)
+            (declare-network test
+                (declare-input X Real [3])
+                (declare-output Y Real [1])
+            )
+            (assert (and (<= X[0] 10.0) (>= X[1] 5.0) (<= X[2] 20.0)))
+            """
+            parse_query_str(content) do (ast)
+                dnf = to_dnf(get_bool(ast))
+                expected = [[ "(<= X [0] 10.0) ", "(>= X [1] 5.0) ", "(<= X [2] 20.0) "]]
+                assert_dnf_equals(expected, dnf)
+            end
+        end
+    end
+    @testset "Disjunction Tests" begin
+        @testset "test_simple_disjunction" begin
+            content = """
+            (vnnlib-version <2.0>)
+            (declare-network test
+                (declare-input X Real [2])
+                (declare-output Y Real [1])
+            )
+            (assert (or (<= X[0] 10.0) (>= X[1] 5.0)))
+            """
+            parse_query_str(content) do (ast)
+                dnf = to_dnf(get_bool(ast))
+                expected = [[ "(<= X [0] 10.0) "], ["(>= X [1] 5.0) "]]
+                assert_dnf_equals(expected, dnf)
+            end
+        end
+        @testset "test_three_way_disjunction" begin
+            content = """
+            (vnnlib-version <2.0>)
+            (declare-network test
+                (declare-input X Real [3])
+                (declare-output Y Real [1])
+            )
+            (assert (or (<= X[0] 10.0) (>= X[1] 5.0) (<= X[2] 0.0)))
+            """
+            parse_query_str(content) do (ast)
+                dnf = to_dnf(get_bool(ast))
+                expected = [[ "(<= X [0] 10.0) "], ["(>= X [1] 5.0) "], ["(<= X [2] 0.0) "]]
+                assert_dnf_equals(expected, dnf)
+            end
+        end
+        @testset "test_and_of_ors" begin
+            content = """
+            (vnnlib-version <2.0>)
+            (declare-network test
+                (declare-input X Real [3])
+                (declare-output Y Real [3])
+            )
+            (assert (and (or (<= X[0] 10.0) (>= X[1] 5.0)) 
+                        (or (<= X[2] 20.0) (>= Y[0] 15.0))
+                        (or (<= Y[1] 30.0) (>= Y[2] 25.0))))
+            """
+            parse_query_str(content) do (ast)
+                dnf = to_dnf(get_bool(ast))
+                expected = [
+                    [ "(<= X [0] 10.0) ", "(<= X [2] 20.0) ", "(<= Y [1] 30.0) "],
+                    [ "(<= X [0] 10.0) ", "(<= X [2] 20.0) ", "(>= Y [2] 25.0) "],
+                    [ "(<= X [0] 10.0) ", "(>= Y [0] 15.0) ", "(<= Y [1] 30.0) "],
+                    [ "(<= X [0] 10.0) ", "(>= Y [0] 15.0) ", "(>= Y [2] 25.0) "],
+                    [ "(>= X [1] 5.0) ", "(<= X [2] 20.0) ", "(<= Y [1] 30.0) "],
+                    [ "(>= X [1] 5.0) ", "(<= X [2] 20.0) ", "(>= Y [2] 25.0) "],
+                    [ "(>= X [1] 5.0) ", "(>= Y [0] 15.0) ", "(<= Y [1] 30.0) "],
+                    [ "(>= X [1] 5.0) ", "(>= Y [0] 15.0) ", "(>= Y [2] 25.0) "]
+                ]
+                assert_dnf_equals(expected, dnf)
+            end
+        end
+    end
+    @testset "Complex Nested Expressions" begin
+        @testset "test_nested_expression" begin
+            content = """
+            (vnnlib-version <2.0>)
+            (declare-network test
+                (declare-input X Real [4])
+                (declare-output Y Real [2])
+            )
+            (assert (and (or (<= X[0] 10.0) (and (>= X[1] 5.0) (<= X[2] 15.0))) 
+                        (or (>= Y[0] 20.0) (<= Y[1] 25.0))))
+            """
+            parse_query_str(content) do (ast)
+                dnf = to_dnf(get_bool(ast))
+                expected = [
+                    [ "(<= X [0] 10.0) ", "(>= Y [0] 20.0) "],
+                    [ "(<= X [0] 10.0) ", "(<= Y [1] 25.0) "],
+                    [ "(>= X [1] 5.0) ", "(<= X [2] 15.0) ", "(>= Y [0] 20.0) "],
+                    [ "(>= X [1] 5.0) ", "(<= X [2] 15.0) ", "(<= Y [1] 25.0) "]
+                ]
+                assert_dnf_equals(expected, dnf)
+            end
+        end
+    end
+    @testset "test_complex_arithmetic" begin
+        content = """
+        (vnnlib-version <2.0>)
+        (declare-network test
+            (declare-input X Real [2])
+            (declare-output Y Real [1])
+        )
+        (assert (and (<= (+ X[0] X[1]) 10.0) (>= (* -1.0 X[0]) -5.0)))
+        """
+        parse_query_str(content) do (ast)
+            dnf = to_dnf(get_bool(ast))
+            expected = [
+                [ "(<= (+ X [0] X [1]) 10.0) ", "(>= (* -1.0 X [0]) -5.0) "]
+            ]
+            assert_dnf_equals(expected, dnf)
         end
     end
     
